1d252054859aa2bd9284d91f7ba2ed9c59c09c49
zahidhasandipu <zahidhasandipu@gmail.com>
2021-07-15 21:50:08 -0700

    update

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem () {
+    return (
+        <div>
+        <div>July 14 2021</div>
+        <div>
+           <h2>Insurance</h2>
+           <div>$458</div>
+           </div>
+        </div>
+        
+    );
+}
+
+export default ExpenseItem;
