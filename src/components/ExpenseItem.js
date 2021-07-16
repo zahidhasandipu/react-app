67f541b1c68eb9991abfce302254f61556ba5d06
zahidhasandipu <zahidhasandipu@gmail.com>
2021-07-16 00:05:53 -0700

    new commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
-function ExpenseItem () {
+import './ExpenseItem.css';
+
+
+function ExpenseItem (props) {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Insurance';
+    const expenseAmount = 458;
+
     return (
-        <div>
-        <div>July 14 2021</div>
-        <div>
-           <h2>Insurance</h2>
-           <div>$458</div>
+        <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+           <h2>{props.title}</h2>
+           <div className="expense-item__price">${props.amount}</div>
            </div>
         </div>
         
