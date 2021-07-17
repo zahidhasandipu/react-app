465ff3773c6168542e7e241b00aa67d9e37ace6e
zahidhasandipu <zahidhasandipu@gmail.com>
2021-07-16 22:00:17 -0700

    update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,33 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 
-function ExpenseItem (props) {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Insurance';
-    const expenseAmount = 458;
-
+const ExpenseItem = (props) => {
     return (
-        <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-        <div className="expense-item__description">
-           <h2>{props.title}</h2>
-           <div className="expense-item__price">${props.amount}</div>
-           </div>
-        </div>
+        <Card className='expense-item'>
+          <ExpenseDate date={props.date} />
+          <div className='expense-item__description'>
+          <h2>{PROPERTY_TYPES.title}</h2>
+          <div className='ex[ense-item__price'>${props.amount}</div>
+          </div>
+          <button>Change Title</button>
+          
+          </Card>
+    // const expenseDate = new Date(2021, 2, 28);
+    // const expenseTitle = 'Insurance';
+    // const expenseAmount = 458;
+
+    // return (
+    //     <div className="expense-item">
+    //     <div>{expenseDate.toISOString()}</div>
+    //     <div className="expense-item__description">
+    //        <h2>{props.title}</h2>
+    //        <div className="expense-item__price">${props.amount}</div>
+    //        </div>
+    //     </div>
         
     );
 }
